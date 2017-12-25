c86deb3362d981ec0200fa5d2e743d849e7be22a
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2017-12-25 09:26:17 +0900

    initial commit

---
+++ lib/gpx_parser.ex
@@ -0,0 +1,18 @@
+defmodule GpxParser do
+  @moduledoc """
+  Documentation for GpxParser.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GpxParser.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gpx_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule GpxParserTest do
+  use ExUnit.Case
+  doctest GpxParser
+
+  test "greets the world" do
+    assert GpxParser.hello() == :world
+  end
+end
