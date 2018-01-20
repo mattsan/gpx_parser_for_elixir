b52f1855637f7a2f9ca8cd55163a0b35ac8972b7
Eiji MATSUMOTO <e.mattsan@gmail.com>
2018-01-20 22:50:39 +0900

    parse track points

--- lib/gpx_parser.ex
+++ lib/gpx_parser.ex
@@ -3,16 +3,28 @@ defmodule GpxParser do
   Documentation for GpxParser.
   """
 
+  import SweetXml
+
+  def parse(gpx) do
+    gpx
+    |> stream_tags(:trkpt)
+    |> Enum.map(&track_point/1)
+  end
+
   @doc """
-  Hello world.
+  トラックポイントの要素を取り出す。
 
-  ## Examples
-
-      iex> GpxParser.hello
-      :world
-
+  取り出す要素は 軽度、緯度、高度、時刻、速度 の五つ。
   """
-  def hello do
-    :world
+  def track_point({:trkpt, trkpt}) do
+    xpath(
+      trkpt,
+      ~x"//trkpt",
+      lat: ~x"./@lat",
+      lon: ~x"./@lon",
+      ele: ~x"./ele/text()",
+      time: ~x"./time/text()",
+      speed: ~x"./speed/text()"
+    )
   end
 end
--- test/gpx_parser_test.exs
+++ test/gpx_parser_test.exs
@@ -2,7 +2,9 @@ defmodule GpxParserTest do
   use ExUnit.Case
   doctest GpxParser
 
-  test "greets the world" do
-    assert GpxParser.hello() == :world
+  describe "parse" do
+    test "empty" do
+      assert GpxParser.parse("<?xml version=\"1.0\" encoding=\"UTF-8\"?><gpx></gpx>") == []
+    end
   end
 end
